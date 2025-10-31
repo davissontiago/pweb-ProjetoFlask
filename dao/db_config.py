14eeb6bc0c49bbb8c510d75d9d806b549861a3cf
davissontiago <davissontiagolf@gmail.com>
2025-10-31 18:42:23 -0300

    Adicionando tramaneto de erro, conexão banco de dados

--- dao/db_config.py
+++ dao/db_config.py
@@ -1,8 +1,16 @@
 import psycopg2
 import os
+from psycopg2 import OperationalError 
 
 # path / url de conexão
 DB_PATH = os.environ.get("DATABASE_URL")
 
 def get_connection():
-    return psycopg2.connect(DB_PATH)
+    if not DB_PATH:
+        raise ConnectionError("Variável de ambiente 'DATABASE_URL' não definida no ambiente de execução.")
+
+    try:
+        return psycopg2.connect(DB_PATH)
+    except OperationalError as e:
+        print(f"ERRO CRÍTICO DE CONEXÃO: {e}")
+        raise ConnectionError("Falha na conexão com o banco de dados. Verifique logs do Vercel para detalhes.")
