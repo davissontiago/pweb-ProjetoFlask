c50604a4c7d0022530980b6306f1c751ba353ce2
davissontiago <davissontiagolf@gmail.com>
2025-10-31 10:39:09 -0300

    banco postgres

--- dao/db_config.py
+++ dao/db_config.py
@@ -1,7 +1,7 @@
-import sqlite3
+import psycopg2
 
 # path / url de conexão
-DB_PATH = "banco_escola.db"
+DB_PATH = "postgresql://neondb_owner:npg_m0ptxo7OaWAU@ep-orange-king-ahtsu5cn-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require"
 
 def get_connection():
-    return sqlite3.connect(DB_PATH)
+    return psycopg2.connect(DB_PATH)
