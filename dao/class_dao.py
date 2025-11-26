bc005e6de38a975306ab35b420b6e7907a380966
davissontiago <davissontiagolf@gmail.com>
2025-11-26 17:15:51 -0300

    Final

--- dao/class_dao.py
+++ dao/class_dao.py
@@ -2,7 +2,7 @@ from dao.db_config  import get_connection
 
 class ClassDAO: 
 
-    sqlSelect = 'SELECT t.id, t.semestre, c.nome_curso, p.nome FROM turma AS t JOIN professor AS p ON t.professor_id = p.id JOIN curso AS c ON t.curso_id = c.id ORDER BY id ASC'
+    sqlSelect = 'SELECT t.id, t.semestre, c.nome_curso, p.nome, p.disciplina FROM turma AS t JOIN professor AS p ON t.professor_id = p.id JOIN curso AS c ON t.curso_id = c.id ORDER BY id ASC'
 
     def get_all(self): 
         conn = get_connection() 
